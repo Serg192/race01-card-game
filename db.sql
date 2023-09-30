3618915b87e97ca9e006d8299fcb1a8f68dc8cf7
Serg192 <sergiystogniy23@gmail.com>
2023-09-30 19:12:21 +0300

    db.sql changes

--- db.sql
+++ db.sql
@@ -13,3 +13,33 @@ CREATE TABLE IF NOT EXISTS users (
     user_role ENUM('admin', 'user') DEFAULT 'user',
     PRIMARY KEY(id)
 );
+
+CREATE TABLE IF NOT EXISTS account_details (
+    user_id INT UNSIGNED NOT NULL,
+    user_picture VARCHAR(10) DEFAULT '1.png',
+    user_coins INT UNSIGNED DEFAULT 100,
+    user_wins INT UNSIGNED DEFAULT 0,
+    user_losses INT UNSIGNED DEFAULT 0,
+    PRIMARY KEY(user_id),
+    FOREIGN KEY(user_id) REFERENCES users(id) ON DELETE CASCADE
+);
+
+CREATE TABLE IF NOT EXISTS cards (
+    id INT UNSIGNED AUTO_INCREMENT,
+    card_name VARCHAR(40) UNIQUE NOT NULL,
+    card_class ENUM('tankman', 'healer', 'dps') NOT NULL,
+    card_attack INT UNSIGNED NOT NULL,
+    card_defence INT UNSIGNED NOT NULL,
+    card_usage_price INT UNSIGNED NOT NULL,
+    card_price INT UNSIGNED NOT NULL,
+    card_picture VARCHAR(30) UNIQUE NOT NULL,
+    PRIMARY KEY (id)
+);
+
+CREATE TABLE IF NOT EXISTS user_cards (
+    user_id INT UNSIGNED NOT NULL,
+    card_id INT UNSIGNED NOT NULL,
+    PRIMARY KEY(user_id, card_id),
+    FOREIGN KEY(user_id) REFERENCES users(id) ON DELETE CASCADE,
+    FOREIGN KEY (card_id) REFERENCES cards(id) ON DELETE CASCADE
+);
