2cda30c84c212fecb06630edb7d0b7b7c018a7d7
Anastasiia Yefimenko <ayefimenko@stud.khpi.ucode-connect.study>
2023-10-02 00:54:22 +0300

    Some changes in lobby were added

--- route/lobby-route.js
+++ route/lobby-route.js
@@ -3,20 +3,36 @@ const path = require("path");
 const express = require("express");
 const router = express.Router();
 
-router.get("/role", (req, res) => {
+const AccountDetails = require("../models/account-details");
+
+router.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname, "../views", "lobby.html"));
+});
+
+router.get("/login", (req, res) => {
   const decoded = jwt.decode(req.cookies.token);
   res.json({
-    role: decoded.role,
+    login: decoded.login,
   });
+});
+
+router.get("/profile_picture", async (req, res) => {
+  const userID = jwt.decode(req.cookies.token).id;
+
+  const userStats = await new AccountDetails().findBy("user_id", userID);
+
+  if (userStats && userStats.length > 0) {
+    const userPicture = userStats[0].user_picture;
+    
+    res.json({
+      profile_picture: userPicture,
+    });
+  }
 });
 
 router.post("/logout", (req, res) => {
   res.clearCookie("token");
   return res.status(200).redirect("/");
-});
-
-router.get("/", (req, res) => {
-    res.sendFile(path.join(__dirname, "../views", "lobby.html"));
 });
 
 module.exports = router;
