80023a9608274df5e5ce8b6c641a9850d698f6c1
Serg192 <sergiystogniy23@gmail.com>
2023-10-02 11:32:52 +0300

    accout-details get full info

--- route/lobby-route.js
+++ route/lobby-route.js
@@ -3,20 +3,13 @@ const path = require("path");
 const express = require("express");
 const router = express.Router();
 
-router.get("/role", (req, res) => {
-  const decoded = jwt.decode(req.cookies.token);
-  res.json({
-    role: decoded.role,
-  });
-});
-
 router.post("/logout", (req, res) => {
   res.clearCookie("token");
   return res.status(200).redirect("/");
 });
 
 router.get("/", (req, res) => {
-    res.sendFile(path.join(__dirname, "../views", "lobby.html"));
+  res.sendFile(path.join(__dirname, "../views", "lobby.html"));
 });
 
 module.exports = router;
